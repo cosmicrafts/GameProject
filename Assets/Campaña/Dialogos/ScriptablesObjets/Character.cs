e3b20022b64526baa5bacf8a7a927d493984dc3c
sakunix <ssakuragi@gmail.com>
2022-05-03 01:01:15 -0600

    dialogos 0.00001

---
+++ Assets/Campaña/Dialogos/ScriptablesObjets/Character.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Nuevo Personaje", menuName = "Personaje")]
+
+public class Character : ScriptableObject
+{
+    public string fullName;
+    public Sprite portrait;
+}
