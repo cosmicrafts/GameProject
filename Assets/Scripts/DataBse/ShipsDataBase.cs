a848f74a1f681f07735842b6876c61fb710da24a
SuryArts <43394702+surycr@users.noreply.github.com>
2022-05-27 13:04:58 -0600

    update
    update

--- Assets/Scripts/DataBse/ShipsDataBase.cs
+++ Assets/Scripts/DataBse/ShipsDataBase.cs
@@ -3,68 +3,117 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName =("Nueva Nave") ,menuName =("Crear Nueva Nave"))]
-public class ShipsDataBase : MonoBehaviour
+public class ShipsDataBase : ScriptableObject
 {
+
     #region DataBase
-    [Header("Asignar el ID")]
+    //----me base en el string correspondiente al GameCard-----------------------------------------------------------
+    [Header("Asignar el NFT KEY para la carta")]
+     [Tooltip("Es el identificador del NFT")]
     [SerializeField]
-    protected int id;
-    [Header("Asignar el ID")]
+    protected string nftsKey;
+
+    //----me base en el string correspondiente al GameCard-----------------------------------------------------------
+    [Tooltip("Informa de la Nombre de la carta")]
+    [Header("Asignar el nombre para la carta")]
     [SerializeField]
-    protected int playerId;
-    [Header("Asignar el ID")]
+    protected string cardName;
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Informa el HP de la carta")]
+    [Header("Asignar el HitPoint de la carta")]
+    [Range(1, 9999)]
     [SerializeField]
     protected int hitPoints;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Informa el MP de la carta")]
+    [Header("Asignar el Max Hp de la carta")]
+    [Range(1, 9999)]
     [SerializeField]
     protected int maxHp;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Informa el Escudo de la carta")]
+    [Header("Asignar el Escudo de la carta")]
     [SerializeField]
+    [Range(1, 9999)]
     protected int shield;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Informa el Maximo  de escudo ")]
+    [Header("Asignar el Maximo de escudo")]
     [SerializeField]
     protected int maxShield;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+
+    [Tooltip("Asignar el delay para el escudo")]
+    [Header("Asignar el delay para el escudo")]
     [SerializeField]
+    [Range(1, 10)]
     protected float shieldDelay;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Asignar la energia para el escudo")]
+    [Header("Asignar la energia requerida")]
     [SerializeField]
+    [Range(1, 9999)]
     protected  int energy;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Asignar la tamaño para el escudo")]
+    [Header("Asignar el tamaño para la nave")]
     [SerializeField]
+    [Range(0, 10)]
     protected float size;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Asignar la area de buff para el zona invocion")]
+    [Header("Asignar el tamaño para el buff de la zona de invocacion")]
     [SerializeField]
+    [Range(0, 30)]
     protected float spawnAreaSize;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Asignar el valor para el tiempo de carga del escudo")]
+    [Header("Asignar el valor para el tiempo de carga del escudo")]
     [SerializeField]
     protected float shieldLoad = 0f;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Asignar el valor para tiempo de recarga del escudo")]
+    [Header("Asignar el valor para tiempo de recarga del escudo")]
     [SerializeField]
     protected float shieldCharge = 0f;
-    [Header("Asignar el ID")]
+
+    //----me base en el int correspondiente al Unit-----------------------------------------------------------
+    [Tooltip("Asignar el valor para la velocidad  del escudo")]
+    [Header("Asignar el valor para la velocidad  del escudo")]
     [SerializeField]
     protected float shieldSpeed = 1f;
+
     #endregion
 
     #region Variables de Lectura
 
-    public int ID => id; //Consulta el ID
-    public int PlayerID => playerId; //Consultar el Player ID
-    public int HitPoins => hitPoints;//Consultar el Player hitPoints Iniciales
-    public int MaxHp => maxHp;
+    public string NFTKEY => nftsKey; //Consulta el  nftsKey; 
 
-    public int Shield => shield;
+    public string CardName => cardName; //Consultar el Player ID ;
+    public int HitPoins => hitPoints;//Consultar el hitPoints ;
+    public int MaxHp => maxHp;//Consultar el  MaxHp Iniciales ;
 
-    public int MaxShield => maxShield;
+    public int Shield => shield; //Consultar el Escudo hitPoints ;
 
-    public float ShieldDelay => shieldDelay;
-    public int Energy => energy;
-    public float Size => size;
+    public int MaxShield => maxShield;//Consultar el Maximo Escudo  ;
 
-    public float SpawnAreaSize => spawnAreaSize;
-    public float ShieldLoad => shieldLoad;
-    public float ShieldCharge => shieldCharge;
-    public float SshieldSpeed => shieldSpeed;
+    public float ShieldDelay => shieldDelay;//Consultar el Delay Escudo ;
+    public int Energy => energy;//Consultar el consumo de energia ;
+    public float Size => size;//Consultar el Tamaño ;
+
+    public float SpawnAreaSize => spawnAreaSize;//Consultar el tamaño para el buff de area de invocacion ;
+    public float ShieldLoad => shieldLoad;//Consultar el la carga del escudo ;
+    public float ShieldCharge => shieldCharge;//Consultar la recarga del escudo;
+    public float ShieldSpeed => shieldSpeed;//Consultar la velocidad del escudo  ;
     #endregion
 }
