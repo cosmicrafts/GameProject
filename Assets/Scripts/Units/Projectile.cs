bac56f80093b731af87289c91822e57b88002e9e
MoisesLopezp <moicano324@hotmail.com>
2022-06-14 17:20:09 -0500

    Mas comentarios de codigo

--- Assets/Scripts/Units/Projectile.cs
+++ Assets/Scripts/Units/Projectile.cs
@@ -1,44 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+
+/*
+ * This is the bullet code (shooted by ships or stations)
+ * Controls the movement and the damage done to the target
+ */
 
 public class Projectile : MonoBehaviour
 {
+    //The bullet team
     [HideInInspector]
     public Team MyTeam;
 
+    //The enemy target to reach
     GameObject Target;
 
+    //The speed of the bullet
     [HideInInspector]
     public float Speed;
 
+    //The damage to done on the target
     [HideInInspector]
     public int Dmg;
 
+    //The game object effect to instantiate when the bullet impacts directly
     public GameObject Inpact;
+    //The game object effect to instantiate when the bullet impacts on shield
     public GameObject ShieldInpact;
 
+    //The last valid position of the target
     Vector3 LastTargetPosition;
+    //A fake bullet is used for multiplayer, to show a visual representation of the original bullet in master
     bool IsFake;
 
     private void Update()
     {
+        //If this bullet is fake
         if (IsFake)
         {
-            //transform.position = Vector3.Lerp(transform.position, LastTargetPosition, Time.deltaTime * 1f/(Speed*0.1f));
+            //Move to the target smoothly
             transform.position = Vector3.MoveTowards(transform.position, LastTargetPosition, Time.deltaTime * Speed);
-        } else
+        } else //A real bullet
         {
+            //Move to the target
             transform.position += transform.forward * Time.deltaTime * Speed;
         }
 
+        //If we have a valid target
         if (Target != null)
         {
+            //Update the last target position and rotate to the target
             LastTargetPosition = Target.transform.position;
             Rotate(Target.transform.position);
-        } else
+        } else //If we lost the target
         {
+            //Rotate to the target
             Rotate(LastTargetPosition);
+            //If the bullet comes to close, destroy the bullet
             if (Vector3.Distance(transform.position, LastTargetPosition) < 1f)
             {
                 transform.position = LastTargetPosition;
@@ -48,59 +65,75 @@ public class Projectile : MonoBehaviour
         }
     }
 
+    //Collisions
     private void OnTriggerEnter(Collider other)
     {
+        //If this is a fake bullet, dont do nothing
         if (IsFake)
             return;
 
+        //Check if the other object is the bullet´s target
         if (other.gameObject == Target)
         {
+            //Impact
             Unit target = Target.GetComponent<Unit>();
             Impact(target);
             return;
         }
 
+        //If the other object is a unit...
         if (other.CompareTag("Unit"))
         {
+            //And is an enemy unit...
             Unit target = other.gameObject.GetComponent<Unit>();
             if (!target.IsMyTeam(MyTeam))
             {
+                //Impact
                 Impact(target);
             }
-        } else if (other.CompareTag("Out"))
+        } else if (other.CompareTag("Out")) //If the bullets go out of the map...
         {
+            //Destroy it
             Destroy(gameObject);
         }
     }
 
+    //Target Impact
     void Impact(Unit target)
     {
-        if (target.Shield > 0)
+        if (target.Shield > 0) //Check if the target has shield
         {
+            //Instantiate the shield impact
             GameObject si = Instantiate(ShieldInpact, transform.position, Quaternion.identity);
             si.transform.LookAt(target.transform);
             Destroy(si, 0.5f);
         }
         else
         {
+            //Instantiate the direct impact
             Destroy(Instantiate(Inpact, transform.position + (transform.forward * 1.5f), Quaternion.identity), 0.5f);
         }
+        //Add damage to the target
         target.AddDmg(Dmg);
         target.SetImpactPosition(transform.position);
+        //Destroy the bullet
         Destroy(gameObject);
     }
 
+    //Rotate the bullet looking to the target
     void Rotate(Vector3 target)
     {
         Quaternion _lookRotation = Quaternion.LookRotation((target - transform.position).normalized);
         transform.rotation = Quaternion.Slerp(transform.rotation, _lookRotation, Time.deltaTime * Speed);
     }
 
+    //Set the last target position
     public void SetLastPosition(Vector3 lastposition)
     {
         LastTargetPosition = lastposition;
     }
 
+    //Set the current enemy target
     public void SetTarget(GameObject target)
     {
         if (target == null)
@@ -113,6 +146,7 @@ public class Projectile : MonoBehaviour
         }
     }
 
+    //Set the bullet as fake
     public void SetFake(bool isfake)
     {
         IsFake = isfake;
--- Assets/Scripts/Units/Shooter.cs
+++ Assets/Scripts/Units/Shooter.cs
@@ -1,53 +1,74 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
+/*
+ * This is the Unit Shooter controller
+ * Detects enemys and shoot bullets to attack them
+ */
+
 public class Shooter : MonoBehaviour
 {
+    //The enemys detector collider
     public SphereCollider EnemyDetector;
 
+    //The unit can attack?
     [HideInInspector]
     public bool CanAttack = true;
 
+    //Range of the enemys detector collider
     [Range(1,99)]
     public float RangeDetector = 1f;
 
+    //Cooldown between shoots
     [Range(0, 99)]
     public float CoolDown = 1f;
 
+    //Bullets speed
     [Range(1,99)]
     public float BulletSpeed = 10f;
 
+    //Bullets damage
     [Range(1, 99)]
     public int BulletDamage = 1;
 
+    //Enables if the unit must look at the enemy target
     public bool RotateToEnemy = true;
 
+    //Enables if the unit must stop his movement if founds an enemy
     public bool StopToAttack = true;
 
+    //The bullet game object to shoot
     public GameObject Bullet;
 
+    //The relative position of the cannons
     public Transform Cannons;
 
+    //The shooting particle systems
     ParticleSystem[] MuzzleFlash;
 
+    //Current cool down for next shoot
     float DelayShoot = 0f;
 
+    //The ship code reference
     Ship MyShip;
 
+    //The unit code reference
     Unit MyUnit;
 
+    //The current in range enemys
     List<Unit> InRange;
 
+    //Current shooting target
     Unit Target;
 
+    //Current fake shooting target
     Unit FakeTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        //SetFakeTarget(GameMng.GM.Targets[0]);
+        //Initialize variables
         InRange = new List<Unit>();
         EnemyDetector.radius = RangeDetector;
         MyShip = GetComponent<Ship>();
@@ -64,30 +85,40 @@ public class Shooter : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Check if this unit is fake
         if (MyUnit.getIsFake())
         {
+            //Fake shoot (multiplayer client)
             ShootFakeTarget();
         } else
         {
+            //Normal shoot
             ShootTarget();
         }
     }
 
+    //Shoot
     public void ShootTarget()
     {
+        //If the unit is death or cant attack or doesn´t has control, cancel shooting
         if (MyUnit.GetIsDeath() || !CanAttack || !MyUnit.InControl())
             return;
 
+        //If we have a target...
         if (Target != null)
         {
+            //If the unit must look at the enemy...
             if (RotateToEnemy)
             {
+                //Rotate to look the enemy
                 var _direction = (Target.transform.position - transform.position).normalized;
                 transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(_direction), Time.deltaTime * 3f);
             }
 
+            //Check if the cool down is done
             if (DelayShoot <= 0f)
             {
+                //Instantiate a bullet per cannon
                 for (int i = 0; i < Cannons.childCount; i++)
                 {
                     Transform cannon = Cannons.GetChild(i);
@@ -99,37 +130,47 @@ public class Shooter : MonoBehaviour
                     MuzzleFlash[i].Clear();
                     MuzzleFlash[i].Play();
                 }
+                //Reset cooldown
                 DelayShoot = CoolDown;
             }
-            else
+            else //Cool down is active
             {
+                //Reduce cool down
                 DelayShoot -= Time.deltaTime;
             }
         }
-
+        //If we dont have a current target
         if (Target == null)
         {
+            //Check if we dont have an other enemy in range
             if (InRange.Count == 0)
             {
+                //Clear the targets
                 InRange.Clear();
                 SetTarget(null);
             }
-            else
+            else //We have more enemys in range
             {
+                //Check for other enemy
                 CleanEnemys();
             }
         }
     }
 
+    //Fake Shooting
     public void ShootFakeTarget()
     {
+        //If the unit is death or cant attack or doesn´t has control, cancel shooting
         if (MyUnit.GetIsDeath() || MyUnit.GetIsDisabled() && MyUnit.GetIsCasting())
             return;
 
+        //If we have a target...
         if (FakeTarget != null)
         {
+            //Check if the cool down is done
             if (DelayShoot <= 0f)
             {
+                //Instantiate a fake bullet per cannon
                 for (int i = 0; i < Cannons.childCount; i++)
                 {
                     Transform cannon = Cannons.GetChild(i);
@@ -142,44 +183,57 @@ public class Shooter : MonoBehaviour
                     MuzzleFlash[i].Clear();
                     MuzzleFlash[i].Play();
                 }
+                //Reset cooldown
                 DelayShoot = CoolDown;
             }
-            else
+            else //Cool down is active
             {
+                //Reduce cool down
                 DelayShoot -= Time.deltaTime;
             }
         }
     }
 
+    //Set the current shooting target
     public void SetTarget(Unit target)
     {
+        //Set the target
         Target = target;
 
+        //If this unit cant move or the unit must not stop when found an enemy, exit the function
         if (MyShip == null || !StopToAttack)
             return;
 
+        //The Target is null
         if (Target == null)
         {
+            //Reset the move destination of the ship
             MyShip.ResetDestination();
         } else
         {
+            //Set the ship destination to the enemy
             MyShip.SetDestination(Target.transform.position, RangeDetector);
         }
     }
 
+    //Set the fake target
     public void SetFakeTarget(Unit target)
     {
         FakeTarget = target;
     }
 
+    //Add an enemy in the targets in range list
     public void AddEnemy(Unit enemy)
     {
+        //Check if the enemy already exist in the list
         if (!InRange.Contains(enemy))
             InRange.Add(enemy);
 
+        //If we have a target, exit the function
         if (Target != null)
             return;
 
+        //We dont have a target, check for a target
         if (InRange.Count == 1)
             SetTarget(enemy);
         else
@@ -188,14 +242,18 @@ public class Shooter : MonoBehaviour
         }
     }
 
+    //Remove an enemy from the targets in range list
     public void RemoveEnemy(Unit enemy)
     {
+        //Check if the enemy exist in the list
         if (InRange.Contains(enemy))
             InRange.Remove(enemy);
 
+        //If we have a target, exit the function
         if (Target != null)
             return;
 
+        //We dont have a target, check for a target
         if (InRange.Count == 0)
             SetTarget(null);
         else
@@ -204,6 +262,7 @@ public class Shooter : MonoBehaviour
         }
     }
 
+    //Clean and check for targets
     public void CleanEnemys()
     {
         InRange = InRange.Where(f => f != null).ToList();
@@ -218,6 +277,7 @@ public class Shooter : MonoBehaviour
         }    
     }
 
+    //Stop shooting
     public void StopAttack()
     {
         CanAttack = false;
@@ -225,11 +285,13 @@ public class Shooter : MonoBehaviour
         SetTarget(null);
     }
 
+    //Get the current unit id of the target
     public int GetIdTarget()
     {
         return Target == null ? 0 : Target.getId();
     }
 
+    //Load and initialize stats from NFT data
     public virtual void InitStatsFromNFT(NFTsUnit nFTsUnit)
     {
         if (GameData.DebugMode)
