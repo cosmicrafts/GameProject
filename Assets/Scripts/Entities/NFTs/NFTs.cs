74dbe0dfdc2878c3c0e457e58a674cbe2acb319b
MoisesLopezp <moicano324@hotmail.com>
2022-06-17 22:33:57 -0500

    variable nivel en nft

--- Assets/Scripts/Entities/NFTs/NFTs.cs
+++ Assets/Scripts/Entities/NFTs/NFTs.cs
@@ -1,11 +1,13 @@
 ﻿//This is the master NFT class
 public abstract class NFTs
 {
-    public string KeyId { get; set; } //* Chr_1 - Chr_2
+    public string KeyId { get; set; }
 
-    public string Icon { get; set; } //* Character_1 - Character_2
+    public string Icon { get; set; }
 
     public int Rarity { get; set; }
 
-    public string Faction { get; set; } //* Alliance - Spirats
+    public string Faction { get; set; }
+
+    public int Level { get; set; }
 }
