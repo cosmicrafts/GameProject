b483240fc1e03b94389edf64478fd339dfe4f8b9
MoisesLopezp <moicano324@hotmail.com>
2022-06-20 20:07:00 -0500

    Comentarios de controlador de unidades

--- Assets/Scripts/Units/Unit.cs
+++ Assets/Scripts/Units/Unit.cs
@@ -1,12 +1,19 @@
 ﻿using EPOOutline;
 using UnityEngine;
 
+/*
+ * This code is the base controller for any unit (ship or station)
+ * Manages the basic stats and unit behaviors
+ */
+
+//The game teams
 public enum Team
 {
     Blue,
     Red
 }
 
+//Type of damages
 public enum TypeDmg
 {
     Normal,
@@ -16,66 +23,98 @@ public enum TypeDmg
 
 public class Unit : MonoBehaviour
 {
+    //The Id of the unit in the game
     protected int Id;
+    //The NFTs source data
     protected NFTsUnit NFTs;
+    //A fake unit is used for multiplayer, to show a visual representation of the original unit in master
     protected bool IsFake;
+    //This unit is Death
     protected bool IsDeath;
+    //The Player owner
     public int PlayerId = 1;
+    //My game team
     public Team MyTeam;
 
+    //Health Points
     [Range(1, 9999)]
     public int HitPoints = 10;
+    //Max Health Points
     int MaxHp = 10;
+    //Shield points
     [Range(1, 9999)]
     public int Shield = 0;
+    //Max Shield points
     int MaxShield = 0;
+    //Shield regeneration delay
     [Range(1, 10)]
     public float ShieldDelay = 3f;
+    //Energy Cost
     [Range(1, 9999)]
     public int Energy = 10;
+    //Size of the unit
     [Range(0, 10)]
     public float Size = 1f;
+    //Spawn area size for other units
     [Range(0, 30)]
     public float SpawnAreaSize = 0f;
 
+    //Returns if this unit is the base station
     [HideInInspector]
     bool IsBaseStation = false;
+    //Main Station Source code (can be null)
     [HideInInspector]
     MainStation MainStationData;
 
+    //This unit can't get damage
     [HideInInspector]
     public bool IsInmortal = false;
 
+    //This unit is disabled (can´t do anything)
     [HideInInspector]
     protected bool Disabled = false;
+    //Cast Delay
     [HideInInspector]
     protected float Casting = 1f;
 
+    //Variables for shield local manage
     float ShieldLoad = 0f;
     float ShieldCharge = 0f;
     float ShieldSpeed = 1f;
  
+    //Trigger for logical detections
     protected SphereCollider TrigerBase;
+    //Trigger for physical detections
     protected SphereCollider SolidBase;
 
+    //Mesh of the unit
     public GameObject Mesh;
+    //Spawn area object reference
     public GameObject SA;
+    //Explosion game object reference (used when the unit is destroyed)
     public GameObject Explosion;
+    //Portal game object reference (Used when the unit is casting)
     public GameObject Portal;
+    //The unit UI reference code
     public UIUnit UI;
+    //The outline shader effect reference
     protected Outlinable MyOutline;
+    //The Animator controller reference
     [SerializeField]
     protected Animator MyAnim;
-
+    //Last bullet impact recived
     protected Vector3 LastImpact;
 
+    //RigidBody Reference
     protected Rigidbody MyRb;
 
+    //The retation target when the unit is fake
     Quaternion FakeRotation;
 
     // Start is called before the first frame update
     virtual protected void Start()
     {
+        //Initialize te variables
         MainStationData = GetComponent<MainStation>();
         IsBaseStation = MainStationData != null;
         LastImpact = Vector3.zero;
@@ -85,17 +124,25 @@ public class Unit : MonoBehaviour
         MyOutline = Mesh.GetComponent<Outlinable>();
         TrigerBase = GetComponent<SphereCollider>();
         SolidBase = Mesh.GetComponent<SphereCollider>();
+        //Initialize UI
         UI.Init(MaxHp - 1, MaxShield - 1);
         UI.SetColorBars(!IsMyTeam(GameMng.P.MyTeam));
+        //Set the outline color
         MyOutline.OutlineParameters.Color = GameMng.GM.GetColorUnit(MyTeam, PlayerId);
+        //Initialize more variables
         TrigerBase.radius = SolidBase.radius;
         transform.localScale = new Vector3(Size, Size, Size);
         MyAnim = Mesh.GetComponent<Animator>();
         Portal.transform.parent = null;
+        //Set the start rotation
         transform.LookAt(CMath.LookToY(transform.position, GameMng.GM.GetFinalTarget(MyTeam).position));
+        //Active the spawn area if the size is more than 0
         SA.SetActive(IsMyTeam(GameMng.P.MyTeam) && SpawnAreaSize > 0f);
+        //Destroy the cast portal after 3 sec
         Destroy(Portal, 3f);
+        //Add the unit at the game manager
         GameMng.GM.AddUnit(this);
+        //Initlialize as fake unit if this unit is fake
         if (IsFake)
         {
             InitHasFake();
@@ -105,6 +152,7 @@ public class Unit : MonoBehaviour
     // Update is called once per frame
     virtual protected void Update()
     {
+        //Casting time controller
         if (Casting > 0f)
         {
             Casting -= Time.deltaTime;
@@ -114,11 +162,13 @@ public class Unit : MonoBehaviour
             }
         }
 
+        //Fake rotation
         if (FakeRotation != null && IsFake)
         {
             transform.rotation = Quaternion.Lerp(transform.rotation, FakeRotation, Time.time * 0.1f);
         }
 
+        //Shield regeneration controller
         if (ShieldLoad > 0f)
         {
             ShieldLoad -= Time.deltaTime;
@@ -139,6 +189,7 @@ public class Unit : MonoBehaviour
         }
     }
 
+    //Normalize and control the physics boundaries
     virtual protected void FixedUpdate()
     {
         if (MyRb.velocity.magnitude > 0f)
@@ -155,6 +206,7 @@ public class Unit : MonoBehaviour
         }
     }
 
+    //Called when the cast is complete
     virtual protected void CastComplete()
     {
         if (SpawnAreaSize > 0f && MyTeam == GameMng.P.MyTeam)
@@ -164,195 +216,244 @@ public class Unit : MonoBehaviour
         }
     }
 
+    //Add specific kind of damage to the unit
     public void AddDmg(int dmg, TypeDmg typeDmg)
     {
+        //Check the state of the unit (can get damage?)
         if (IsDeath || !InControl() || dmg <= 0)
             return;
 
+        //Reset the shield regeneation delay
         ShieldLoad = ShieldDelay;
         ShieldCharge = 0f;
 
+        //Direct Damage varaible
         int DirectDmg = 0;
 
+        //Check the current shield points
         if (Shield > 0 && typeDmg != TypeDmg.Direct)
         {
+            //Reduce the shield points
             Shield -= dmg;
             if (Shield < 0)
             {
+                //Reduce the HP if the shield points are negative
                 HitPoints += Shield;
                 DirectDmg += Mathf.Abs(Shield);
                 Shield = 0;
             }
+            //Update the unit Sield UI
             UI.SetShieldBar((float)Shield / (float)MaxShield);
         } else if (typeDmg != TypeDmg.Shield)
         {
+            //Direct Damage
             HitPoints -= dmg;
             DirectDmg += dmg;
         }
 
+        //Add the data to the metrics
         if (!IsMyTeam(GameMng.P.MyTeam))
         {
             GameMng.MT.AddDamage(DirectDmg);
         }
 
+        //Check if the unit is already death
         if (HitPoints <= 0 && !IsInmortal)
         {
+            //Die
             HitPoints = 0;
             Die();
         }
 
+        //Update HP UI
         UI.SetHPBar((float)HitPoints / (float)MaxHp);
     }
 
+    //Add normal damage
     public void AddDmg(int dmg)
     {
         AddDmg(dmg, TypeDmg.Normal);
     }
 
+    //Kill the unit
     public virtual void Die()
     {
         if (IsDeath)
             return;
 
+        //Set the variables
         HitPoints = 0;
         IsDeath = true;
+        //Hide UI
         UI.HideUI();
+        //Disable Spawn area
         SA.SetActive(false);
+        //Run death animation
         MyAnim.SetBool("Death", true);
     }
 
+    //Disable Unit
     public virtual void DisableUnit()
     {
         Disabled = true;
 
+        //Disable movement component (if exist)
         Ship ship = GetComponent<Ship>();
 
         if (ship != null)
             ship.CanMove = false;
 
+        //Disable Shooter component (if exist)
         Shooter shooter = GetComponent<Shooter>();
 
         if (shooter != null)
             shooter.CanAttack = false;
     }
 
+    //Enable unit
     public virtual void EnableUnit()
     {
         Disabled = false;
 
+        //Enable movement component (if exist)
         Ship ship = GetComponent<Ship>();
 
         if (ship != null)
             ship.CanMove = true;
 
+        //Enable shooter component (if exist)
         Shooter shooter = GetComponent<Shooter>();
 
         if (shooter != null)
             shooter.CanAttack = true;
     }
 
+    //Check is some team is the same team of this unit
     public bool IsMyTeam(Team other)
     {
         return other == MyTeam;
     }
 
+    //Get is the unit is death
     public bool GetIsDeath()
     {
         return IsDeath;
     }
 
+    //Get if the unit is disabled
     public bool GetIsDisabled()
     {
         return Disabled;
     }
 
+    //Get if the unit is casting
     public bool GetIsCasting()
     {
         return Casting > 0f;
     }
 
+    //Destroy the unit (after death animation)
     public void DestroyUnit()
     {
+        //Remove from the game manager
         GameMng.GM.DeleteUnit(this);
 
+        //Instantiate explosion object effect
         GameObject explosion = Instantiate(Explosion, transform.position, Quaternion.identity);
         explosion.transform.localScale = transform.localScale * 1.8f;
         Destroy(explosion, 1f);
 
+        //Chec if is the base station
         if (!GameMng.GM.IsGameOver() && IsBaseStation)
         {
+            //End the game
             GameMng.GM.EndGame(MyTeam == Team.Blue ? Team.Red : Team.Blue);
         }
 
+        //Add metrics data
         if (!IsMyTeam(GameMng.P.MyTeam))
         {
             GameMng.MT.AddKills(1);
         }
 
+        //Destroy the object
         Destroy(gameObject);
     }
 
+    //Set the last impact position recived
     public void SetImpactPosition(Vector3 position)
     {
         LastImpact = position;
     }
 
+    //Set the unit ID
     public void setId(int id)
     {
         Id = id;
     }
 
+    //Get the unit ID
     public int getId()
     {
         return Id;
     }
 
+    //Get the NFT key
     public string getKey()
     {
         return NFTs.KeyId;
     }
 
+    //Get the owners player ID
     public int GetPlayerId()
     {
         return PlayerId;
     }
 
+    //Get if this unit is fake
     public bool getIsFake()
     {
         return IsFake;
     }
 
+    //Set this unit as fake
     public void setHasFake()
     {
         IsFake = true;
     }
 
+    //Initialize this unit as fake
     void InitHasFake()
     {
         TrigerBase.enabled = false;
         SolidBase.enabled = false;
     }
 
+    //Set the fake rotation
     public void SetFakeRotation(Quaternion quaternion)
     {
         FakeRotation = quaternion;
     }
 
+    //Get the unit animator controller
     public Animator GetAnimator()
     {
         return MyAnim;
     }
 
+    //Get the max shield points
     public int GetMaxShield()
     {
         return MaxShield;
     }
 
+    //Set the max shield points
     public void SetMaxShield(int maxshield)
     {
         MaxShield = maxshield;
     }
 
+    //Set the shield data as fake data
     public void SetFakeShield(int sh, int maxshield)
     {
         if (!IsFake)
@@ -363,12 +464,20 @@ public class Unit : MonoBehaviour
         UI.SetShieldBar((float)sh / (float)maxshield);
     }
 
+    //Set the max HP 
     public void SetMaxHitPoints(int maxhp)
     {
         MaxHp = maxhp;
         UI.SetHPBar((float)HitPoints / (float)MaxHp);
     }
 
+    //Get the max HP
+    public int GetMaxHitPoints()
+    {
+        return MaxHp;
+    }
+
+    //Set the HP data as fake data
     public void SetFakeHp(int hp, int maxhp)
     {
         if (!IsFake)
@@ -384,26 +493,26 @@ public class Unit : MonoBehaviour
         UI.SetHPBar((float)hp / (float)maxhp);
     }
 
-    public int GetMaxHitPoints()
-    {
-        return MaxHp;
-    }
-
+    //Set the NFT data source
     public virtual void SetNfts(NFTsUnit nFTsUnit)
     {
         NFTs = nFTsUnit;
 
+        //Validate the data
         if (GameData.DebugMode || nFTsUnit == null)
             return;
 
+        //Remplace the variables
         HitPoints = nFTsUnit.HitPoints;
         MaxHp = HitPoints;
         Shield = nFTsUnit.Shield;
         MaxShield = Shield;
 
+        //Initialize the data in the shooter controller (if exist)
         GetComponent<Shooter>()?.InitStatsFromNFT(nFTsUnit);
     }
 
+    //Returns if the unit is in his normal state
     public bool InControl()
     {
         return (!Disabled && Casting <= 0f && !IsFake);
