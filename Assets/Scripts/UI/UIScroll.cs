3638118db4b8efd1ec33009345aead19cc7d3eee
erickpicos <erickjosepicos@gmail.com>
2022-06-30 16:48:51 -0300

    Corrección FX - SPI (Blue) - ALL (Red)

--- Assets/Scripts/UI/UIScroll.cs
+++ Assets/Scripts/UI/UIScroll.cs
@@ -8,11 +8,14 @@ public class UIScroll : MonoBehaviour
     private float Ref = 768f;
     public float timeAnimation = 0.5f;
 
+    private int column = 0;
+    private int row = 0;
+    
     public void MoveTo(string item = "0,0")
     {
         string[] split = item.Split(",");
-        int column = int.Parse(split[0]);
-        int row = int.Parse(split[1]);
+        column = int.Parse(split[0]);
+        row = int.Parse(split[1]);
         Debug.Log("Column: "+column+"  Row: "+ row);
            
         h = Screen.height;
@@ -37,4 +40,31 @@ public class UIScroll : MonoBehaviour
            
         transform.localPosition = targetPosition;
     }
+    
+    // Resizing functions here
+    private void ScreenResized() {
+        Vector3 start = transform.localPosition;
+        float targetX = (w / h) * Ref * column;
+        float targetY =  Ref * row;
+        Vector3 targetPosition = new Vector3(targetX, targetY, transform.localPosition.z);
+        
+        transform.localPosition = targetPosition;
+    }
+
+    // On every screen refresh
+    void Update()
+    {
+        if ((Screen.width != w) || (Screen.height != h)) {
+            ScreenResized();
+            w = Screen.width;
+            h = Screen.height;
+        }
+    }
+    
+    
+    
+    
 }
+
+
+
