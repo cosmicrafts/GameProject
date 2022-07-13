621491439fea9222a708200a1bed8c6f358bd1a1
SuryArts <43394702+surycr@users.noreply.github.com>
2022-07-13 16:57:20 -0600

    fixed black screen y que se vea blanco el sprite si no hay nft
    fixed black screen y que se vea blanco el sprite si no hay nft

--- Assets/Scripts/UI/UIMainMenu.cs
+++ Assets/Scripts/UI/UIMainMenu.cs
@@ -30,10 +30,8 @@ public class UIMainMenu : MonoBehaviour
     public GameObject CollectionMenu;
     public GameObject CharactersMenu;
     public GameObject GameModesMenu;
-    // Intro Door
-
-    [SerializeField]
-    Animator doorAnim;
+   
+  
     //Back button 
     public GameObject BackBtn;
 
@@ -63,6 +61,7 @@ public class UIMainMenu : MonoBehaviour
     int UserDataLoaded;
     int isFirstLogin =0;
     public GameObject welcomePanel;
+    public Sprite noNftSprite;
     private void Awake()
     {
 
@@ -226,7 +225,7 @@ public class UIMainMenu : MonoBehaviour
         StartCoroutine(LoadNFTsIcons());
         if (!GlobalManager.GMD.DebugMode)
             LoadingPanel.instance.DesactiveLoadingPanel();
-        doorAnim.SetTrigger("DoorIntro");
+      
         MenuPanel.SetActive(true);
     }
 
@@ -457,6 +456,10 @@ public class UIMainMenu : MonoBehaviour
                 yield return www.SendWebRequest();
                 Texture2D webTexture = ((DownloadHandlerTexture)www.downloadHandler).texture;
                 card.IconSprite = Sprite.Create(webTexture, new Rect(0.0f, 0.0f, webTexture.width, webTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+            }
+            else
+            {
+                card.IconSprite = noNftSprite;
             }
         }
 
