67c5505b291fde5e166e66bed264cdc6cb14a406
erickpicos <32298145+erickpicos@users.noreply.github.com>
2023-08-24 02:22:20 -0300

    Skills character 2

--- Assets/Scripts/Characters/Character_2.cs
+++ Assets/Scripts/Characters/Character_2.cs
@@ -10,9 +10,10 @@ public class Character_2 : GameCharacter
         Shooter shooter = unit.GetComponent<Shooter>();
         if (shooter != null)
         {
+            shooter.criticalStrikeChance += 0.1f; //Increment Chance critical strike by 10% (Skill 1)
             shooter.BulletDamage = (int)(shooter.BulletDamage * 1.2f); //Increment Damage bullet by 20% (Skill 2)
         }
-        
+        unit.DodgeChance += 0.1f; //Increment dodge chance in 100% (skill 3)
     }
     
 }
