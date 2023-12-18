af4c39d612ab97afe15762bd505ebeef6b3ed9f0
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2023-12-18 04:55:19 -0600

    fix
    daba errores por los scripts pero ya no los esoty usando

--- Assets/Scripts/SO/Requirements.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-
-[System.Serializable]
-public class Requirements
-{
-    public Dictionary<string, string> Tokens;
-    public List<string> NFTs;
-}
