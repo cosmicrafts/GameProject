e68007523aa6292400c9a6a2332d2edf203604d9
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-07-29 18:44:45 -0600

    gamecard update

--- Assets/Scripts/Units/UnitCard.cs
+++ Assets/Scripts/Units/UnitCard.cs
@@ -1,55 +1,10 @@
-﻿using Unity.Entities;
-using Unity.Transforms;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class EnergySystem : SystemBase
+/*
+ * Extension of a game card for units (with a mesh)
+ */
+
+public class UnitCard : GameCard
 {
-    protected override void OnUpdate()
-    {
-        float deltaTime = Time.DeltaTime;
-
-        // Update player energy
-        Entities.ForEach((ref PlayerComponent player) =>
-        {
-            if (player.CurrentEnergy < player.MaxEnergy)
-            {
-                player.CurrentEnergy += player.SpeedEnergy * deltaTime;
-            }
-        }).Schedule();
-
-        // Update bot enemy energy
-        Entities.ForEach((ref BotEnemyComponent bot) =>
-        {
-            if (bot.CurrentEnergy < bot.MaxEnergy)
-            {
-                bot.CurrentEnergy += bot.SpeedEnergy * deltaTime;
-            }
-        }).Schedule();
-    }
-}
-
-public class HealthSystem : SystemBase
-{
-    protected override void OnUpdate()
-    {
-        Entities.ForEach((ref HealthComponent health, in PlayerComponent player) =>
-        {
-            // Example logic for updating health
-            if (health.HitPoints <= 0 && !health.IsDeath)
-            {
-                health.IsDeath = true;
-                Debug.Log("Player is dead");
-            }
-        }).Schedule();
-
-        Entities.ForEach((ref HealthComponent health, in BotEnemyComponent bot) =>
-        {
-            // Example logic for updating health
-            if (health.HitPoints <= 0 && !health.IsDeath)
-            {
-                health.IsDeath = true;
-                Debug.Log("Bot is dead");
-            }
-        }).Schedule();
-    }
+    public GameObject UnitMesh;
 }
